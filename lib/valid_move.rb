1bf24ad074da9d62df0289877a804736ae2a2f6c
MJU <marie.ju2012@outlook.com>
2020-01-30 23:36:00 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -10,5 +10,3 @@ end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
   return !(board[index] == " " || board[index] == "" || board[index] == nil)
-
-
