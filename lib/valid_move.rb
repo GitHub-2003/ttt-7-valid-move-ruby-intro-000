7d88977e41cd46b415b5027ba0317ef2fe6cb1fd
MJU <marie.ju2012@outlook.com>
2020-01-30 23:52:02 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if index < 0 and index > 8
+  if index < 0 || index > 8
     return false
   else
     return !(position_taken?(board, index))
