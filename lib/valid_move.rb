a02db027bd42144e83ccdacf29e1e74fe17796ad
MJU <marie.ju2012@outlook.com>
2020-01-30 23:48:13 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,7 +7,6 @@ def valid_move?(board, index)
   end
 end
 
-
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
   return !(board[index] == " " || board[index] == "" || board[index] == nil)
@@ -15,6 +14,7 @@ end
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 index = 0
+position_taken?(board, index)
 valid_move?(board, index)
 
 board = [" ", " ", "X", " ", " ", " ", " ", "O", " "]
